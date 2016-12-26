9cc5fcfc36e8676d76cfbdd80c97ea83da0a1016
Sumedh Nimkarde <blesssumedh@gmail.com>
2016-12-26 10:23:23 +0530

    change the title to address

--- public/js/googlemaps.js
+++ public/js/googlemaps.js
@@ -1,95 +1,41 @@
-var config = {
-        apiKey: "AIzaSyDqcidTNKiWrY_n2rl-J8xpoLBetUY0IiM",
-        authDomain: "g-mapster-1482049698790.firebaseapp.com",
-        databaseURL: "https://g-mapster-1482049698790.firebaseio.com",
-        storageBucket: "g-mapster-1482049698790.appspot.com",
-        messagingSenderId: "392450380738"
-    };
+var accuratebtn = document.querySelector('#accuratebtn');
 
-    firebase.initializeApp(config);
+accuratebtn.addEventListener('click', function(){
+  getMyLocation();
+});
 
-    //Get the upload button
-    const filebutton = document.querySelector('#pushbtn');
-    filebutton.addEventListener('change', function(e){
-      //Get the file
-      console.log('button clicked for file upload');
-        var file = e.target.files[0];
-      //Create storage refernce for it
-        var storageRef = firebase.storage().ref('g-mapster/' + file.name );
-      //Upload
-        var task = storageRef.put(file);
-        task.on('state_changed',
-          function progress(s){
-            var upload = s.bytesTransferred/s.totalBytes;
-            console.log(upload);
-            if(upload == 1){
-              var downloadURL = task.h.downloadURLs[0];
-              console.log(downloadURL);
-              saveGarbagePlace(downloadURL);
-            }
-          }
-        );
-    });
-
-    // Save the coordinates
-
-    function saveGarbagePlace(imageURL){
-      if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position){
-          console.log(position);
-          firebase.database().ref('/').push({
-            latitude : position.coords.latitude,
-            longitude : position.coords.longitude,
-            imageURL : imageURL
-          });
-          console.log('inserted');
-          markTheMap(position);
-        });
-      }
-      else
-        alert('Your device doesnt support geolocation');
-    }
-
-    // Mark the map
-
-    function markTheMap(position){
-      console.log('markTheMap map');
-      var map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 4,
-              center: { lat : position.coords.latitude-5, lng : position.coords.longitude-5 }
-          });
-          var ref = firebase.database().ref('/');
-          ref.orderByChild('latitude').on('child_added', function(snapshot){
-            console.log(snapshot.val().latitude);
-            console.log(snapshot.val().longitude);
-            var location = { lat : snapshot.val().latitude, lng : snapshot.val().longitude };
-            placeMarker(location, map);
-          });
-    }
-
-    function placeMarker(location, map){
-      var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            icon : 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            title : 'Hello World!'
-          });
-    }
-
-    function initMap(){
-      if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position){
-        var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: { lat : position.coords.latitude-5, lng : position.coords.longitude-5 }
-            });
-            var ref = firebase.database().ref('/');
-            ref.orderByChild('latitude').on('child_added', function(snapshot){
-              console.log(snapshot.val().latitude);
-              console.log(snapshot.val().longitude);
-              var location = { lat : snapshot.val().latitude, lng : snapshot.val().longitude };
-              placeMarker(location, map);
-        });
+function getMyLocation(location){
+  if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function(position){
+      // console.log(location);
+      var latlng = new google.maps.LatLng( location.lat, location.lng );
+      var geocoder = new google.maps.Geocoder();
+      formatted_address = geocoder.geocode({ location : latlng }, function(results, status){
+        console.log(results[0]['formatted_address']);
+        return results[0]['formatted_address'];
       });
-    }
+    });
   }
+}
+
+function displayResults(results, status){
+  console.log(results[0]['formatted_address']);
+  return results[0]['formatted_address']
+}
+
+function geolocation(){
+console.log('geolocation called');
+var loc = {};
+var geocoder = new google.maps.Geocoder();
+if(google.loader.ClientLocation){
+  loc.lat = google.loader.ClientLocation.latitude;
+  loc.lng = google.loader.ClientLocation.longitude;
+}
+var latlng = new google.maps.LatLng(loc.lat, loc.lng);
+      geocoder.geocode({'latLng': latlng}, function(results, status) {
+          if(status == google.maps.GeocoderStatus.OK) {
+              // alert(results[0]['formatted_a0ddress']);
+              console.log(results[0]['formatted_address']);
+          };
+      });
+}
