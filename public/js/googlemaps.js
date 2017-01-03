cc63693e436cf24f3927efa83d6ee8831220dc0b
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-01-03 15:19:11 +0530

    added infowindows to maps

--- public/js/googlemaps.js
+++ public/js/googlemaps.js
@@ -114,13 +114,14 @@ function initMap(){
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
       map = new google.maps.Map(document.getElementById('map'), {
-        zoom : 10,
-        center : { lat : 28.38, lng : 77.12 }
+        zoom : 16
       });
       var centermarker = new google.maps.Marker({
         position : { lat : position.coords.latitude, lng : position.coords.longitude },
-        map : map
+        title : 'You are here!',
+        map : map,
       });
+      map.setCenter({ lat : position.coords.latitude, lng : position.coords.longitude });
     });
     retrieveAndPlace();
   }
@@ -133,16 +134,28 @@ function initMap(){
 var ref = firebase.database().ref('/');
 function retrieveAndPlace(){
   ref.orderByChild('latitude').on('child_added', function(snapshot){
-    console.log(snapshot.val());
-    placeMarker({ lat : snapshot.val().latitude, lng : snapshot.val().longitude }, snapshot.val().landmark);
+    // console.log(snapshot.val());
+    placeMarker(snapshot.val());
   });
 }
 
-function placeMarker(location, landmark){
+function placeMarker(object){
   var marker = new google.maps.Marker({
-    position : location,
+    position : { lat : object.latitude, lng : object.longitude },
     map : map,
-    title : String(landmark),
-    animation : google.maps.Animation.DROP
+    title : String(object.landmark)
+  });
+  marker.addListener('click', function(){
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function(){
+      marker.setAnimation(null);
+    }, 1500);
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content : object.landmark + '\n' + '<a href = ' + object.imageURL + ' target = "blank" >' + 'See the image' +  '</a>'
+  });
+  marker.addListener('click', function(){
+    infowindow.open(map, marker);
   });
 }
